c93b9c13e00e7faa5b56061ee37fa121b340793c
maxmax <maxbakaiev@gmail.com>
2023-12-27 00:09:24 +0200

    docker-compose up, client fix

--- sequelize-server-client/server/app/config/db.config.js
+++ sequelize-server-client/server/app/config/db.config.js
@@ -6,12 +6,12 @@ module.exports = {
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
-  dialect:  process.env.DB_DIALECT,
+  dialect: process.env.DB_DIALECT,
   port: process.env.DB_PORT,
   pool: {
     max: 5,
     min: 0,
     acquire: 30000,
-    idle: 10000
-  }
+    idle: 10000,
+  },
 };
